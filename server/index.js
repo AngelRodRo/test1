11e595092a4ebbd1b585d89b6a60a32d43c60a59
angel <aleangrodriguez123@gmail.com>
2020-11-20 07:36:13 -0500

    fix: code styling

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const cors = require('cors');
+require("dotenv").config();
+
+const app = require("express")();
+const http = require("http").createServer(app);
+const cors = require("cors");
+
+const { version } = require("./package.json");
+
+const routes = require("./routes");
 
 const PORT = 3001;
 
 app.use(cors());
 
-app.get('/', (_, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.json({
+    name: "git-logs-api",
+    version
+  });
 });
 
+app.use("/commits", routes);
 
 http.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
