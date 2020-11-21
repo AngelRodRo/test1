8b270e0f7a464334e21dff36c252977c46a61b9d
Angel Rodriguez <aleangrodriguez123@gmail.com>
2020-11-21 17:01:09 -0500

    feature: add better style structure for commit component

--- client/src/components/commit.tsx
+++ client/src/components/commit.tsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
+import Typography from '@material-ui/core/Typography'
+
 import Styled from 'styled-components'
 
 import { Commit } from '../models/commit'
 
+import Author from './author'
+
 const StyledCard = Styled(Card)`
     max-width: 650px;
     width: 100%;
+    margin: 10px 0;
+    padding: 20px;
 `
 
 function CommitCard(props: Commit) {
-    const { sha, committer, message } = props
-
+    const { sha, committer, commit } = props
     return (
         <StyledCard>
-            {sha}
-            {committer.avatar_url}
-            {committer.url}
-            {message}
+            <Typography variant="h6">
+                {commit.message}
+            </Typography>
+            <Author
+                name={commit.author.name}
+                avatar={committer.avatar_url}
+                url={committer.html_url}
+            />
         </StyledCard>
     )
 }
+
+CommitCard.componentName = 'Commit'
 
 export default CommitCard
