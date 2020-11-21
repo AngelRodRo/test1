254a713aa04bee40d03935b0bbf6a588f6e0014f
Angel Rodriguez <aleangrodriguez123@gmail.com>
2020-11-21 17:39:49 -0500

    feature: commit component styling

--- client/src/components/commit.tsx
+++ client/src/components/commit.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import LaunchIcon from '@material-ui/icons/Launch'
 
 import Styled from 'styled-components'
 
@@ -13,20 +15,40 @@ const StyledCard = Styled(Card)`
     width: 100%;
     margin: 10px 0;
     padding: 20px;
+    position: relative;
+`
+
+const StyledLink = Styled(Link)`
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 10px !important;
+`
+
+const StyledInfo = Styled.div`
+    margin: 10px;
 `
 
 function CommitCard(props: Commit) {
-    const { sha, committer, commit } = props
+    const { sha, committer, commit, html_url } = props
     return (
-        <StyledCard>
-            <Typography variant="h6">
-                {commit.message}
-            </Typography>
+        <StyledCard variant="outlined">
+            <StyledInfo>
+                <Typography variant="h6">
+                    {commit.message}
+                </Typography>
+                <Typography variant="caption">
+                    {sha}
+                </Typography>
+            </StyledInfo>
             <Author
                 name={commit.author.name}
                 avatar={committer.avatar_url}
                 url={committer.html_url}
             />
+            <StyledLink href={html_url}>
+                <LaunchIcon />
+            </StyledLink>
         </StyledCard>
     )
 }
