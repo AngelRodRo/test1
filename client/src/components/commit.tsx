f2109a762b2d1b03e495a97e994d7da90a4690e7
Angel Rodriguez <aleangrodriguez123@gmail.com>
2020-11-21 08:58:14 -0500

    chore: commit component styling

--- client/src/components/commit.tsx
+++ client/src/components/commit.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
+import Styled from 'styled-components'
 
-import { Commit } from '../models/commit';
+import { Commit } from '../models/commit'
+
+const StyledCard = Styled(Card)`
+    max-width: 650px;
+    width: 100%;
+`
 
 function CommitCard(props: Commit) {
     const { sha, committer, message } = props
 
     return (
-        <Card>
+        <StyledCard>
             {sha}
             {committer.avatar_url}
             {committer.url}
             {message}
-        </Card>
+        </StyledCard>
     )
 }
 
