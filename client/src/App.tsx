523f01deda2dbed1629a2d3b28fa571834427566
Angel Rodriguez <aleangrodriguez123@gmail.com>
2020-11-21 08:58:57 -0500

    chore: commit list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Styled from 'styled-components'
+
+import Commit from './components/commit'
 
 function App() {
+  const commits = [{
+    sha: 'asdas',
+    message: 'asdasd',
+    committer: {
+      avatar_url: 'asdda',
+      url: 'adssa'
+    }
+  }]
+
+  const commitsList = commits.map(({ sha, message, committer }) => 
+    <Commit
+      sha={sha}
+      message={message}
+      committer={committer}
+    />
+  )
+
+  const StyledList = Styled.div`
+    display: flex;
+    justify-content: center;
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledList>
+      {commitsList}
+    </StyledList>
+  )
 }
 
-export default App;
+export default App
