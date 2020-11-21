db9782f2d3980d46a2d485af2edd2b6893899816
Angel Rodriguez <aleangrodriguez123@gmail.com>
2020-11-21 17:37:21 -0500

    feature: add html_url for commit in response

--- server/controllers/git.js
+++ server/controllers/git.js
@@ -8,10 +8,10 @@ const getGitPublicURL = () =>
 module.exports.getCommits = async (_, res) => {
   try {
     const { data: commits } = await axios.get(getGitPublicURL());
-    const formatedCommits = commits.map(({ sha, commit, url, committer }) => ({
+    const formatedCommits = commits.map(({ sha, commit, html_url, committer }) => ({
       sha,
       commit,
-      url,
+      html_url,
       committer
     }));
     res.json(formatedCommits);
